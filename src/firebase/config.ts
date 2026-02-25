f4bf08ea508312e7b0e75e47c3c5bde8e96ce8f7
Krishna mehta <mehtakrishna985@gmail.com>
2026-02-25 23:18:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-27028371-3d113",
+  "appId": "1:665965835202:web:603b02a6a8f2ba975c5c5b",
+  "apiKey": "AIzaSyC7F3gFfSQYvSEoqgHh6ypyQHtbQIVCyB0",
+  "authDomain": "studio-27028371-3d113.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "665965835202"
+};
